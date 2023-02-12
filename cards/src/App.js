fe8a8e0661758dfd1280aed2d01f019c7d1951c1
elenaturlenko <turlenkoelena@gmail.com>
2023-02-12 19:00:11 +0500

    ДЗ недели 27

---
+++ cards/src/App.js
@@ -0,0 +1,28 @@
+import tariffs from "./data/data.json";
+import {TariffCard} from "./componets/card/card";
+import './App.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+            <div className="main">
+                <div className="main__container">
+                    {
+                        tariffs.map((tariff) =>
+                            <TariffCard
+                                key={tariff.id}
+                                tariff={tariff.tariff}
+                                price={tariff.price}
+                                speed={tariff.speed}
+                                theme={tariff.theme}
+                                isSelected={tariff.isSelected}>
+                            </TariffCard>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default App;
